b965d6e4f03c334626ed77687346befdf2fb7fee
Marlon Grazek <business.marlongrazek@gmail.com>
2022-12-09 13:58:50 +0100

    Initial commit

---
+++ src/main/java/com/marlongrazek/ui/UI.java
@@ -0,0 +1,17 @@
+package com.marlongrazek.ui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
